278e7a42c0c11fd8ebd878d078ec543f8cb0811a
danilogalisteu <danilogalisteu@gmail.com>
2024-04-10 11:40:24 -0300

    basic server withuot handling

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/danilogalisteu/bd-07-gp-chirpy
+
+go 1.22.1
---
+++ main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
+
+func middlewareCors(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE")
+		w.Header().Set("Access-Control-Allow-Headers", "*")
+		if r.Method == "OPTIONS" {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+		next.ServeHTTP(w, r)
+	})
+}
+
+func main() {
+	mux := http.NewServeMux()
+	corsMux := middlewareCors(mux)
+	server := http.Server{
+		Addr:    "localhost:8080",
+		Handler: corsMux,
+	}
+	err := server.ListenAndServe()
+	if err == http.ErrServerClosed {
+		fmt.Printf("server closed\n")
+	} else if err != nil {
+		fmt.Printf("error starting server: %s\n", err)
+		os.Exit(1)
+	}
+}
