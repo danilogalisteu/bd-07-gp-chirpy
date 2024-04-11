fc89a7e7582aac856242920a76011e136be7ce65
danilogalisteu <danilogalisteu@gmail.com>
2024-04-11 15:37:15 -0300

    health endpoint

--- main.go
+++ main.go
@@ -21,12 +21,19 @@ func middlewareCors(next http.Handler) http.Handler {
 
 func main() {
 	mux := http.NewServeMux()
-	mux.Handle("/", http.FileServer(http.Dir(".")))
+	mux.Handle("/app/*", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
+	mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("OK"))
+	})
+
 	corsMux := middlewareCors(mux)
 	server := http.Server{
 		Addr:    "localhost:8080",
 		Handler: corsMux,
 	}
+
 	err := server.ListenAndServe()
 	if err == http.ErrServerClosed {
 		fmt.Printf("server closed\n")
