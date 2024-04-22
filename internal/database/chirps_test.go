6e1682fcb5ae91dde164f62d3809978332760c80
danilogalisteu <danilogalisteu@gmail.com>
2024-04-22 12:28:45 -0300

    added tests for different author

--- internal/database/chirps_test.go
+++ internal/database/chirps_test.go
@@ -1,6 +1,7 @@
 package database
 
 import (
+	"fmt"
 	"os"
 	"testing"
 )
@@ -93,6 +94,33 @@ func TestDBChirps(t *testing.T) {
 
 	if chirps[0].ID == deleted_chirp_id {
 		t.Errorf("ID in the DB ('%d') shouldn't match the deleted ID ('%d')", chirps[0].ID, deleted_chirp_id)
+	}
+
+	chirp_new_author_id := 2
+	new_author_chirp_text := "new author test chirp"
+	chirp, err = db.CreateChirp(chirp_new_author_id, new_author_chirp_text)
+	if err != nil {
+		t.Errorf("Error creating message on DB:\n%v", err)
+	}
+
+	t.Logf("New chirp: ID = %d, body = '%s'", chirp.ID, chirp.Body)
+
+	chirps, err = db.GetChirps()
+	if err != nil {
+		t.Fatalf("Error getting messages from DB:\n%v", err)
+	}
+	fmt.Println(chirps)
+
+	if len(chirps) != 2 {
+		t.Fatalf("The messages DB should have length 2 instead of %d", len(chirps))
+	}
+
+	if chirps[1].AuthorID != chirp_new_author_id {
+		t.Errorf("Content in the DB ('%d') doesn't match the input author ('%d')", chirps[1].AuthorID, chirp_new_author_id)
+	}
+
+	if chirps[1].Body != new_author_chirp_text {
+		t.Errorf("Content in the DB ('%s') doesn't match the input text ('%s')", chirps[1].Body, new_author_chirp_text)
 	}
 
 }
