23b2c572252bd75f66d25e70580c5f623b527d41
Danilo Galisteu <danilogalisteu@gmail.com>
2025-04-26 10:55:42 -0300

    added column is_chirpy_red to users

--- internal/database/update_user.sql.go
+++ internal/database/update_user.sql.go
@@ -16,7 +16,7 @@ const updateUser = `-- name: UpdateUser :one
 UPDATE users
 SET email = $2, hashed_password = $3, updated_at = $4
 WHERE id = $1
-RETURNING id, created_at, updated_at, email, hashed_password
+RETURNING id, created_at, updated_at, email, hashed_password, is_chirpy_red
 `
 
 type UpdateUserParams struct {
@@ -40,6 +40,7 @@ func (q *Queries) UpdateUser(ctx context.Context, arg UpdateUserParams) (User, e
 		&i.UpdatedAt,
 		&i.Email,
 		&i.HashedPassword,
+		&i.IsChirpyRed,
 	)
 	return i, err
 }
